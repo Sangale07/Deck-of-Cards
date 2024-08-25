99cf48a5789f01fa1c521b83b2f620551575a3b7
rushi <rushikeshsangale57@gmail.com>
2024-08-25 19:22:23 +0530

    DeckofCards Class Created

--- src/Main.java
+++ src/Main.java
@@ -4,3 +4,19 @@ public class Main {
 
     }
 }
+ class DeckOfCards {
+     private static final String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
+     private static final String[] RANKS = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+     private static final int NUM_OF_CARDS = SUITS.length * RANKS.length;
+     private String[] deck = new String[NUM_OF_CARDS];
+
+     public DeckOfCards() {
+         // Initialize deck
+         int index = 0;
+         for (String suit : SUITS) {
+             for (String rank : RANKS) {
+                 deck[index++] = rank + " of " + suit;
+             }
+         }
+     }
+ }
